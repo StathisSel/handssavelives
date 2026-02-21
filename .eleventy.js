1f94b15a36dfe4b4739af81d006329bf6d0cad14
StathisSel <76625518+StathisSel@users.noreply.github.com>
2026-02-21 15:02:57 +0200

    Set up Eleventy configuration with passthrough copies
    Configure Eleventy to copy static assets and set template engines.

--- .eleventy.js
+++ .eleventy.js
@@ -1,1 +1,17 @@
+module.exports = function (eleventyConfig) {
+  // Κράτα τους υπάρχοντες φακέλους σου ως “static”
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("admin");
 
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
+  };
+};
